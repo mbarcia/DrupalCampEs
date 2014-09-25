fa5a05a0d1e57579184b81ac144f77167223cc42
David Langarica Lorenzo <david.langaricalorenzo@gmail.com>
2014-09-25 04:17:44 +0200

    Installed drupal, created profile for drupalcamp, added contrib modules
    Former-commit-id: 1a48a42585336f330fcef48f90aacaffaa6e3a49 [formerly aead7b5106b75e6e10c28fb4b084d2666cec44ca]
    Former-commit-id: 50cd16d9ee7e04245c4e606ee2681af391c4a1bd

---
+++ sites/all/themes/drupalcamp/template.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+/**
+ * Implements theme_process_html().
+ */
+function drupalcamp_preprocess_html(&$vars) {
+  $headers = drupal_get_http_header();
+
+  if (isset($headers['status'])) {
+    $vars['classes_array'][] = 'error-page';
+    if ($headers['status'] == '404 Not Found') {
+      $vars['classes_array'][] = 'error-page-404';
+    }
+    elseif ($headers['status'] == '403 Forbidden') {
+      $vars['classes_array'][] = 'error-page-403';
+    }
+  }
+}
+
+/**
+ * Implements theme_process_page().
+ */
+function drupalcamp_process_page(&$vars) {
+  $vars['navbar_classes'] = 'navbar navbar-default';
+}
+
+/**
+ * Implements theme_image().
+ */
+function drupalcamp_image(&$variables) {
+  $attributes = $variables['attributes'];
+
+  // Add responsive class from bootstrap.
+  $attributes['class'][] = 'img-responsive';
+
+  $attributes['src'] = file_create_url($variables['path']);
+
+  foreach (array('width', 'height', 'alt', 'title') as $key) {
+
+    if (isset($variables[$key])) {
+      $attributes[$key] = $variables[$key];
+    }
+  }
+
+  return '<img' . drupal_attributes($attributes) . ' />';
+}
