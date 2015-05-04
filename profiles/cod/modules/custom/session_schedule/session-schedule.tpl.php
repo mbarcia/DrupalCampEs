a3700d2a334950220661054a35cf7f06717de4c0
Marcelo Tosco <capy.net@gmail.com>
2015-05-04 17:46:01 -0300

    Añado las versiones responsive y la url final del calendario de charlas.
    Former-commit-id: cfed2db279c4fe1d9ec616bb5563e0672c8d2ba1 [formerly 1fa1e66521b09806bbfb471b84493993be664b30]
    Former-commit-id: 047280ca75d3c4125acf8fa428cfa741eb9f1eb9

--- profiles/cod/modules/custom/session_schedule/session-schedule.tpl.php
+++ profiles/cod/modules/custom/session_schedule/session-schedule.tpl.php
@@ -8,6 +8,199 @@
 </div>
 
 <h2>Sabado</h2>
+
+<table class="session-table mobile">
+
+  <tr>
+    <td class="headings">9:00 a 9:30</td>
+    <td class="all-tracks">BIENVENIDA</td>
+  </tr>
+
+  <tr>
+    <td class="headings">9:30 a 10:30</td>
+    <td>
+      <div>
+        <span class="room">Sala 1:</span>
+        FEATURED 1<br/>
+        <span class="speaker">Cathy Theys</span></div>
+      <div class="advanced">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/144">Theming en Drupal 8:<br/>un soplo de aire fresco <br/>
+        </a><span class="speaker">Pako Garcia</span></div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/84">La estrategia de contenidos <br/> ha llegado para quedarse <br/>
+        </a><span class="speaker">Koen Platteeuw</span></div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/85">Drupal 8 for site builders <br/>
+        </a><span class="speaker">MontaÃ±a Franco</span></div>
+      <div>
+        <span class="room">Sala 5:</span>
+        Curso FORCONTU <br/>
+        Parte 1
+      </div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">10:30 a 11:30</td>
+    <td>
+      <div><span class="room">Sala 1:</span>FEATURED 2 <br/>
+        <span class="speaker">Tobias Stockler</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/87">Render API en Drupal 7 & 8 <br/>
+        </a><span class="speaker">Jose Luis Bellido</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/153">
+          No esperes a Drupal 8 para <br/> ampliar tu juego de herramientas <br/>
+        </a><span class="speaker">Salva Molina</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/73">El poder del Talisman <br/>
+        </a><span class="speaker">NeftalÃ­ Guerrero</span>
+      </div>
+      <div>
+        <span class="room">Sala 5:</span>
+        Curso FORCONTU <br/>
+        Parte 1
+      </div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">11:30 a 12:00</td>
+    <td class="all-tracks">COFFEE BREAK</td>
+  </tr>
+
+  <tr>
+    <td class="headings">12:00 a 13:00</td>
+    <td>
+      <div>
+        <span class="room">Sala 1:</span>
+        FEATURED 3 <br/>
+        <span class="speaker">Ricardo</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/72">
+          Drupal 8 Frontend for Backenders <br/>
+        </a><span class="speaker">Lauri Eskola</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/155">
+          Encontrando mi lugar: el paso de desarrollo a direcciÃ³n de proyectos <br/>
+        </a><span class="speaker">Laura Portilla & Patricia Peinado</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/123">
+          Deploy de desarrollos Drupal con Capistrano <br/>
+        </a><span class="speaker">David Gil</span>
+      </div>
+      <div>
+        <span class="room">Sala 5:</span>
+        Curso FORCONTU <br/>
+        Parte 2
+      </div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">13:00 a 14:00</td>
+    <td>
+      <div class="advanced">
+        <span class="room">Sala 1:</span><a href="http://2015.drupalcamp.es/node/111">
+          Drupal Extreme Scaling <br/>
+        </a><span class="speaker">Zeki VÃ¡zquez</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/161">
+          Behat y Drupal Extension <br/>
+        </a><span class="speaker">Ricardo Sanz</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/112">
+          Peer reviews: revisando el cÃ³digo en equipo <br/>
+        </a><span class="speaker">Juampy Novillo & Rodrigo Aguilera</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/89">
+          Comunidad Drupal Â¿Hay vida detrÃ¡s del cÃ³digo? <br/>
+        </a><span class="speaker">MontaÃ±a Franco, Jose Luis Bellido & Francisco Garde</span>
+      </div>
+      <div>
+        <span class="room">Sala 5:</span>
+        Curso FORCONTU <br/>
+        Parte 2
+      </div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">14:00 a 15:30</td>
+    <td class="all-tracks">ALMUERZO</td>
+  </tr>
+
+  <tr>
+    <td class="headings">15:30 a 16:30</td>
+    <td>
+      <div class="advanced">
+        <span class="room">Sala 1:</span><a href="http://2015.drupalcamp.es/node/113">
+          Thinking about starting *now* a project with Drupal 8? Here is my story <br/>
+        </a><span class="speaker">Christian LÃ³pez</span>
+
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/110">
+          Women in tech <br/>
+        </a><span class="speaker">Cathy Theys & MontaÃ±a Franco</span>
+      </div>
+      <div class="advanced">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/88">
+          JavaScript tips & tricks <br/>
+        </a><span class="speaker">Marcelo Tosco</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/151">
+          Â¿QuÃ© mÃ³dulos me instalo? <br/>
+        </a><span class="speaker">Carlos Reig</span>
+      </div>
+      <div><span class="room">Sala 5:</span>SPRINTS</div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">16:30 a 17:30</td>
+    <td>
+    <div class="advanced">
+      <span class="room">Sala 1:</span><a href="http://2015.drupalcamp.es/node/147">
+        D7 as D8 <br/>
+      </a><span class="speaker">Pablo LÃ³pez</span>
+    </div>
+    <div class="beginner">
+      <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/135">
+        TecnologÃ­a y consciencia. Buscando la coherencia entre la persona y el profesional <br/>
+      </a><span class="speaker">NoemÃ­ Rodriguez & Juan Olalla</span>
+    </div>
+    <div class="medium">
+      <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/142">
+        Arquitectura de desarrollo en proyectos Drupal <br/>
+      </a><span class="speaker">Ramon Vilar</span>
+    </div>
+    <div class="beginner">
+      <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/140">
+        Component based design and development <br/>
+      </a><span class="speaker">Cristina Chumillas</span>
+    </div>
+      <div><span class="room">Sala 5:</span>SPRINTS</div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">17:30 a 19:30</td>
+    <td class="all-tracks">ASAMBLEA</td>
+  </tr>
+
+</table>
+
 <table class="session-table">
   <tr class="headings">
     <td>Horario</td>
@@ -49,7 +242,7 @@
         No esperes a Drupal 8 para <br/> ampliar tu juego de herramientas <br/>
       </a><span class="speaker">Salva Molina</span>
     </td>
-    <td  class="beginner"><a href="http://2015.drupalcamp.es/node/73">El poder del Talisman <br/>
+    <td class="beginner"><a href="http://2015.drupalcamp.es/node/73">El poder del Talisman <br/>
       </a><span class="speaker">NeftalÃ­ Guerrero</span>
     </td>
   </tr>
@@ -150,12 +343,129 @@
 </table>
 
 
-
-
-
-
-
 <h2>Domingo</h2>
+<table class="session-table mobile">
+  <tr>
+    <td class="headings">9:30 a 10:30</td>
+    <td>
+      <div><span class="room">Sala 1:</span> ORO 1</div>
+      <div class="advanced">
+        <span class="room">Sala 2:</span> <a href="http://2015.drupalcamp.es/node/128">Los desarrolladores tambiÃ©n somos personas<br/>
+        </a><span class="speaker">Ãlvaro Hurtado</span></div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span> <a href="http://2015.drupalcamp.es/node/66">[Taller] Drupal Architecture Workshop<br/>
+        </a><span class="speaker">David HernÃ¡ndez</span></div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span> <a href="http://2015.drupalcamp.es/node/69">"Ã‘apa certified" - CÃ³mo crear un drupal inmantenible<br/>
+        </a><span class="speaker">Marcos Cano</span></div>
+      <div>
+        <span class="room">Sala 5:</span>
+        SPRINTS
+      </div>
+    </td>
+  </tr>
+
+
+  <tr>
+    <td class="headings">10:30 a 11:30</td>
+    <td>
+      <div><span class="room">Sala 1:</span>ORO 2<br/>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/152">
+          Panels: From Zero to Hero in 2000 seconds<br/>
+        </a><span class="speaker">Salva Molina & Joeri Poesen</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span> <a href="http://2015.drupalcamp.es/node/66">[Taller] Drupal Architecture Workshop<br/>
+        </a><span class="speaker">David HernÃ¡ndez</span></div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/70">Desacoplando Drupal con React y CouchDB<br/>
+        </a><span class="speaker">Juampy Novillo</span>
+      </div>
+      <div>
+        <span class="room">Sala 5:</span>
+        SPRINTS
+      </div>
+    </td>
+  </tr>
+
+
+  <tr>
+    <td class="headings">11:30 a 12:00</td>
+    <td class="all-tracks">
+      <div>COFFEE BREAK</div>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="headings">12:00 a 13:00</td>
+    <td>
+
+      <div><span class="room">Sala 1:</span><a href="http://2015.drupalcamp.es/node/162">Cuando Dev encontrÃ³ a Ops<br/>
+        </a><span class="speaker">Pedro GonzÃ¡lez</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/64">
+          Desacoplar la lÃ³gica de negocio del framework<br/>
+        </a><span class="speaker">Manel LÃ³pez</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/67">
+          [Taller] Drupal 8 multilingual sobre el terreno<br/>
+        </a><span class="speaker">Cathy Theys & Rodrigo Aguilera</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/132">
+          Propuesta de mejora del sistema de pruebas de Drupal<br/>
+        </a><span class="speaker">Ana Belen</span>
+      </div>
+      <div><span class="room">Sala 5:</span>
+        SPRINTS
+      </div>
+
+
+    </td>
+  </tr>
+
+
+  <tr>
+    <td class="headings">13:00 a 14:00</td>
+    <td>
+      <div class="advanced">
+        <span class="room">Sala 1:</span><a href="http://2015.drupalcamp.es/node/119">
+          Formularios en Drupal 8<br/>
+        </a><span class="speaker">Luis Ortiz</span>
+      </div>
+      <div class="medium">
+        <span class="room">Sala 2:</span><a href="http://2015.drupalcamp.es/node/163">
+          When content editors ask for full flexibility: Drupal architectures and patterns<br/>
+        </a><span class="speaker">Hernani Borges</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 3:</span><a href="http://2015.drupalcamp.es/node/67">
+          [Taller] Drupal 8 multilingual sobre el terreno<br/>
+        </a><span class="speaker">Cathy Theys & Rodrigo Aguilera</span>
+      </div>
+      <div class="beginner">
+        <span class="room">Sala 4:</span><a href="http://2015.drupalcamp.es/node/75">
+          Â¿CÃ³mo y por quÃ© contribuir con Drupal?<br/>
+        </a><span class="speaker">Jose Manuel RodrÃ­guez</span>
+      </div>
+      <div><span class="room">Sala 5:</span>
+        SPRINTS
+      </div>
+
+    </td>
+  </tr>
+
+
+  <tr>
+    <td class="headings">14:00 a 14:30</td>
+    <td class="all-tracks">CLAUSURA EVENTO</td>
+  </tr>
+</table>
+
 <table class="session-table">
   <tr class="headings">
     <td>Horario</td>
