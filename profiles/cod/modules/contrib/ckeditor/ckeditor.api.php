bdea68cc08a4e0c8d2ba4ec055841f6735fceb2e
capynet <capy.net@gmail.com>
2015-08-18 15:32:18 +0200

    Añado modulos contrib y actualizo los existentes.
    Former-commit-id: 8e4f0b45baa934fd05c06bf2693aaebbad6ef47e [formerly 2f7959efaa3f11ba6beb0ea3dea143ee5e600398]
    Former-commit-id: 7abb58dfff2a7429f53698d31a8417762e861eb9

--- profiles/cod/modules/contrib/ckeditor/ckeditor.api.php
+++ profiles/cod/modules/contrib/ckeditor/ckeditor.api.php
@@ -45,26 +45,15 @@ function hook_ckeditor_plugin() {
       // Description of the plugin - it would be displayed in the plugins management section of profile settings.
       'desc' => t('Plugin description'),
       // The full path to the CKEditor plugins directory, with the trailing slash.
-      'path' => '/' . drupal_get_path('module', 'my_module') . '/plugin_dir/',
+      'path' => drupal_get_path('module', 'my_module') . '/plugin_dir/',
       'buttons' => array(
         'button_name' => array(
-          'icon' => 'icon/plugin_name.png',
+          'icon' => 'path to button icon',
           'label' => 'Button Label',
         )
       )
     )
   );
-}
-
-/**
- * Hook to extend/change CKEditor plugins.
- *
- * @param $plugins
- *   An associative array of plugins.
- */
-function hook_ckeditor_plugin_alter(&$plugins) {
-  // Remove a plugin button.
-  unset($plugins['plugin_name']['buttons']['button']);
 }
 
 /**
