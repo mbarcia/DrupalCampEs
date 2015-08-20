bd432ca0ccea5cdff0c055fdc81c071b1446db41
casanova <casanova347@gmail.com>
2015-08-20 09:29:02 -0300

    Agrego layout landing.
    Former-commit-id: 39532e37d5fdeefcdd218b07ac96b354630de5c4 [formerly 28f2bb17330a0011c2bba5501bf7f9e31d1ea8e8]
    Former-commit-id: fb9a759d7598e17424902a696c8db7d6f37b5571

---
+++ profiles/cod/themes/dcamp_th/layouts/landing/README.txt
@@ -0,0 +1,5 @@
+Verificar que la linea '@import "../layouts/**/*";' se encuentra en "colias.styles.scss".
+Es para que todos los layout puedan exponer sus archivos .scss
+Agregarla si falta.
+
+Tambien hay que poner "add_import_path 'layouts'" en config.rb para que los watch funcionen.
---
+++ profiles/cod/themes/dcamp_th/layouts/landing/landing-layout.tpl.php
@@ -0,0 +1,63 @@
+<div<?php print $attributes; ?>>
+
+  <?php if (!empty($page['one'])): ?>
+  <div class="l-one">
+    <?php print render($page['one']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['two'])): ?>
+  <div class="l-two">
+    <?php print render($page['two']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['three'])): ?>
+  <div class="l-three">
+    <?php print render($page['three']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['four'])): ?>
+  <div class="l-four">
+    <?php print render($page['four']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['five'])): ?>
+  <div class="l-five">
+    <?php print render($page['five']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['six'])): ?>
+  <div class="l-six">
+    <?php print render($page['six']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['seven'])): ?>
+  <div class="l-seven">
+    <?php print render($page['seven']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['eight'])): ?>
+    <div class="l-eight">
+      <?php print render($page['eight']); ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['nine'])): ?>
+  <div class="l-nine">
+    <?php print render($page['nine']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($page['ten'])): ?>
+  <div class="l-ten">
+    <?php print render($page['ten']); ?>
+  </div>
+  <?php endif; ?>
+
+</div>
